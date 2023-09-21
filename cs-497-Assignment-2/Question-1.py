f857ed6bdb67fce38d2623a49d0f5b402430d81a
josec15 <78322105+josec15@users.noreply.github.com>
2023-09-21 12:22:25 -0700

    added solution to question 1 and explanation to readme file

--- cs-497-Assignment-2/Question-1.py
+++ cs-497-Assignment-2/Question-1.py
@@ -1,1 +1,24 @@
 # Question 1
+# Majority Element
+
+# Given an array nums of size n, return the majority element.
+# The majority element is the element that appears more than [n / 2] times.
+# You may assume that the majority element always exists in the array.
+# Solve this problem in Linear time: O(n).
+
+nums = [3, 2, 3]
+
+majority = 0
+count = 0
+
+for i in nums:
+    if majority == i:
+        count += 1
+    elif count == 0:
+        majority = i
+        count = 1
+    else:
+        count -= 1
+print(majority)
+
+
